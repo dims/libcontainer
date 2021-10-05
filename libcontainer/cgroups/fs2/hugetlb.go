dims/libcontainer

commit 9dbd72427df32b8155cc4c4f0022b4aa62e0d633

Changed hugetlb pagesizes info source
Signed-off-by: Boris Popovschi <zyqsempai@mail.ru>

libcontainer/cgroups/fs2/hugetlb.go
@@ -25,30 +25,34 @@ func setHugeTlb(dirPath string, cgroup *configs.Cgroup) error {
 	return nil
 }
 
-func statHugeTlb(dirPath string, stats *cgroups.Stats, cgroup *configs.Cgroup) error {
+func statHugeTlb(dirPath string, stats *cgroups.Stats) error {
+	hugePageSizes, err := cgroups.GetHugePageSize()
+	if err != nil {
+		return errors.Wrap(err, "failed to fetch hugetlb info")
+	}
 	hugetlbStats := cgroups.HugetlbStats{}
 
-	for _, entry := range cgroup.Resources.HugetlbLimit {
-		usage := strings.Join([]string{"hugetlb", entry.Pagesize, "current"}, ".")
+	for _, pagesize := range hugePageSizes {
+		usage := strings.Join([]string{"hugetlb", pagesize, "current"}, ".")
 		value, err := fscommon.GetCgroupParamUint(dirPath, usage)
 		if err != nil {
-			return errors.Wrapf(err, "failed to parse hugetlb.%s.current file", entry.Pagesize)
+			return errors.Wrapf(err, "failed to parse hugetlb.%s.current file", pagesize)
 		}
 		hugetlbStats.Usage = value
 
-		fileName := strings.Join([]string{"hugetlb", entry.Pagesize, "events"}, ".")
+		fileName := strings.Join([]string{"hugetlb", pagesize, "events"}, ".")
 		filePath := filepath.Join(dirPath, fileName)
 		contents, err := ioutil.ReadFile(filePath)
 		if err != nil {
-			return errors.Wrapf(err, "failed to parse hugetlb.%s.events file", entry.Pagesize)
+			return errors.Wrapf(err, "failed to parse hugetlb.%s.events file", pagesize)
 		}
 		_, value, err = fscommon.GetCgroupParamKeyValue(string(contents))
 		if err != nil {
-			return errors.Wrapf(err, "failed to parse hugetlb.%s.events file", entry.Pagesize)
+			return errors.Wrapf(err, "failed to parse hugetlb.%s.events file", pagesize)
 		}
 		hugetlbStats.Failcnt = value
 
-		stats.HugetlbStats[entry.Pagesize] = hugetlbStats
+		stats.HugetlbStats[pagesize] = hugetlbStats
 	}
 
 	return nil

libcontainer/cgroups/fs2/fs2.go
@@ -126,7 +126,7 @@ func (m *manager) GetStats() (*cgroups.Stats, error) {
 	}
 	// hugetlb (since kernel 5.6)
 	if _, ok := m.controllers["hugetlb"]; ok {
-		if err := statHugeTlb(m.dirPath, &st, m.config); err != nil {
+		if err := statHugeTlb(m.dirPath, &st); err != nil {
 			errs = append(errs, err)
 		}
 	}

commit 49d99b86b0672c9f338f574fcf7457ee5162a0ef

libct/cg/fs2: fix GetStats for unsupported hugetlb
In case hugetlb is not supported, GetStats() should not error out,
and yet it does.

Assume that if GetHugePageSize return an error, hugetlb is
not supported (this is what cgroup v1 manager do).

Fixes: 9dbd7242
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

libcontainer/cgroups/fs2/hugetlb.go
@@ -1,7 +1,6 @@
 package fs2
 
 import (
-	"fmt"
 	"strconv"
 
 	"github.com/opencontainers/runc/libcontainer/cgroups"
@@ -27,10 +26,7 @@ func setHugeTlb(dirPath string, r *configs.Resources) error {
 }
 
 func statHugeTlb(dirPath string, stats *cgroups.Stats) error {
-	hugePageSizes, err := cgroups.GetHugePageSize()
-	if err != nil {
-		return fmt.Errorf("failed to fetch hugetlb info: %w", err)
-	}
+	hugePageSizes, _ := cgroups.GetHugePageSize()
 	hugetlbStats := cgroups.HugetlbStats{}
 
 	for _, pagesize := range hugePageSizes {